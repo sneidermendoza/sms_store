53920f484887c2ef5301a15bb6bf1aa2247102ba
sneider mendoza <mariasol0304@gmail.com>
2022-03-15 23:02:06 -0500

    creasion de slug, slug-automaticos, presave, slug-unicos

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Products
 
-admin.site.register(Products)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'price')
+    list_display = ('__str__', 'slug', 'created_at')
+
+admin.site.register(Products, ProductAdmin)
--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('<pk>', views.ProductDetailview.as_view(), name='product')
+    path('<slug:slug>', views.ProductDetailview.as_view(), name='product')
 ]
